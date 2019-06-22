058949e1f55c6f2f8f59692ac1ad4b96568ece50
Vivek Singh <Vivek Singh@DESKTOP-K1LF7LB>
2019-06-22 14:54:32 +0530

    220619: Exploring

--- MatchCards/MatchCards/Views/ActualMatchCardsPage.xaml.cs
+++ MatchCards/MatchCards/Views/ActualMatchCardsPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MatchCards.Annotations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,42 @@ namespace MatchCards.Views
     /// <summary>
     /// Interaction logic for ActualMatchCardsPage.xaml
     /// </summary>
-    public partial class ActualMatchCardsPage : UserControl
+    public partial class ActualMatchCardsPage : UserControl, System.ComponentModel.INotifyPropertyChanged
     {
         public ActualMatchCardsPage()
         {
             InitializeComponent();
         }
+        private int viewType;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int ViewType
+        {
+            get { return viewType; }
+            set
+            {
+                viewType = value;
+                OnPropertyChanged("ViewType");
+               
+            }
+        }
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void listv_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ViewType = 1;
+        }
+
+        private void listv_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ViewType = 2;
+        }
+
     }
 }
